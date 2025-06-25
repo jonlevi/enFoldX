e01329354f941ca7a769e12d7f52004ef7cfcc9c
jonlevi <jonalevine1@gmail.com>
2025-06-25 17:42:37 -0400

    revert autoformat on non-python file

--- af3_config.sh
+++ af3_config.sh
@@ -1,4 +1,4 @@
-ALPHAFOLD_DIR = "path/to/alphafold3"
-DATABASE_DIR = "path/to/public_databases"
-WEIGHTS_DIR = "path/to/weights"
-CONTAINER_PATH = "path/to/container"
+ALPHAFOLD_DIR="path/to/alphafold3"
+DATABASE_DIR="path/to/public_databases"
+WEIGHTS_DIR="path/to/weights"
+CONTAINER_PATH="path/to/container"
