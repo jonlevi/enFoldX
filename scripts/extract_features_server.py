be90046102ad398730935e48ab927873849835d7
jonlevi <jonalevine1@gmail.com>
2026-07-13 12:32:49 -0400

    fix typo

--- scripts/extract_features_server.py
+++ scripts/extract_features_server.py
@@ -127,7 +127,7 @@ def parse_af3_results(
                     )
                     results[f"min_plddt_{chain_mapper.get(chain1)}"] = np.min(sub_plddt)
                     results[f"max_plddt_{chain_mapper.get(chain1)}"] = np.max(sub_plddt)
-                    results[f"std_plddt_{chain_mapper.get(chain1)}"] = np.max(sub_plddt)
+                    results[f"std_plddt_{chain_mapper.get(chain1)}"] = np.std(sub_plddt)
 
         # CDR3 Metrics
         residues_alpha = [
